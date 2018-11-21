24fba9e2057d7ddc2d34b8ed5c4c56d73a7335f2
cucugbl <gabrielmacs@hotmail.com>
2018-11-21 14:01:06 -0500

    header

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material';
+import { PrincipalComponent } from './principal/principal.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
