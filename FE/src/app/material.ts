22fa362990f10edc6b7708c34adbe72ca329e5a0
Gabriel <you@example.com>
2018-11-22 20:58:36 -0500

    contenedores

--- FE/src/app/material.ts
+++ FE/src/app/material.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatTooltipModule],
-  exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatTooltipModule],
+  imports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatTooltipModule,MatCardModule],
+  exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatTooltipModule,MatCardModule],
 })
 export class MaterialModule { }
